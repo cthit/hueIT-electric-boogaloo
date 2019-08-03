411bbe7c686d999c8d838490bc16750389454f9f
Latte <gustav.lahti@gmail.com>
2019-08-03 16:32:33 +0200

    Backend can now receive a list of RequestBody as well.

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -1,4 +1,3 @@
-import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.client.HttpClient
@@ -9,23 +8,27 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
 import io.ktor.request.receive
 import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.post
-import io.ktor.routing.route
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.json.simple.JSONObject
 import java.io.File
+import kotlin.collections.set
 
 val client = HttpClient()
 val debug = true
+
+data class RequestBodyList(
+    val requestBodyList: List<RequestBody>
+)
 
 data class RequestBody(
     val isGroup: Boolean,
     val id: Int,
     val props: List<RequestBodyProperty>
 )
+
 data class RequestBodyProperty(
     val key: String,
     val value: Int
@@ -40,7 +43,7 @@ fun main(args: Array<String>) {
     val baseURL = "http://" + fileLines[0] + "/api/" + fileLines[1] + "/"
 
     val server = embeddedServer(Netty, port = 8080) {
-        install(ContentNegotiation){
+        install(ContentNegotiation) {
             jackson {
             }
         }
@@ -49,39 +52,63 @@ fun main(args: Array<String>) {
             post("/") {
                 val reqBod = call.receive<RequestBody>()
 
-                if (reqBod.props.isEmpty()){
-                    call.respondText {
-                        client.get{
-                            url(baseURL +
-                                    (if (reqBod.isGroup) "groups" else "lights") +
-                                    (if (reqBod.id >= 0) "/" + reqBod.id else ""))
-                        }
-                    }
-                } else {
-                    val body = JSONObject()
+                call.respondText {
+                    handleRequestBody(reqBod, baseURL)
+                }
+            }
 
-                    reqBod.props.forEach{
-                        when (it.key){
-                            "power" -> body["on"] = it.value != 0
-                            "hue" -> body["hue"] = it.value * 65535 / 360
-                            "sat", "bri" -> body[it.key] = it.value * 254 / 100
-                            "rst" -> {
-                                body["hue"] = 8418
-                                body["bri"] = 254
-                                body["sat"] = 140
-                            }
-                        }
-                    }
+            post("/list") {
+                val reqBods = call.receive<RequestBodyList>()
+                val responses = ArrayList<String>()
 
-                    call.respondText {
-                        statusUpdate(baseURL, body, if (reqBod.isGroup) "groups" else "lights", reqBod.id)
-                    }
+                reqBods.requestBodyList.forEach {
+                    responses.add(handleRequestBody(it, baseURL))
+                }
+
+                val responseBody = JSONObject()
+                responseBody["responses"] = responses
+
+                call.respondText {
+                    responseBody.toString()
                 }
             }
         }
     }
 
     server.start(wait = true)
+}
+
+suspend fun handleRequestBody(reqBod: RequestBody, baseURL: String): String {
+    if (reqBod.props.isEmpty()) {
+        return client.get {
+            url(
+                baseURL +
+                        (if (reqBod.isGroup) "groups" else "lights") +
+                        (if (reqBod.id >= 0) "/" + reqBod.id else "")
+            )
+        }
+    } else {
+        val body = JSONObject()
+
+        reqBod.props.forEach {
+            when (it.key) {
+                "power" -> body["on"] = it.value != 0
+                "hue" -> body["hue"] = it.value * 65535 / 360
+                "sat", "bri" -> body[it.key] = it.value * 254 / 100
+                "rst" -> {
+                    body["hue"] = 8418
+                    body["bri"] = 254
+                    body["sat"] = 140
+                }
+            }
+        }
+
+        val updateResponse = statusUpdate(baseURL, body, if (reqBod.isGroup) "groups" else "lights", reqBod.id)
+
+        println(updateResponse)
+
+        return updateResponse
+    }
 }
 
 suspend fun statusUpdate(baseURL: String, bodyObject: JSONObject, type: String, id: Int): String {
