23a2523a6274c01967f4c91bc47321a4087c2f76
Magnus Wamby <wamby@student.chalmers.se>
2019-09-10 20:19:27 +0200

    Fix preset cards not being removed when the preset is deleted
    This is done by updating an empty state in the presets screen, but
    is not an elegant fix

--- frontend/src/PresetCardComponent.js
+++ frontend/src/PresetCardComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
@@ -12,7 +12,7 @@ import { DeletePreset, LampToHex } from "./Util";
 import IconButton from "@material-ui/core/IconButton";
 
 export default function PresetCardComponent(props) {
-    const { preset, lamps, setLamps } = props;
+    const { preset, lamps, setLamps, parentForceUpdate } = props;
 
     function handleClickApply() {
         setLamps(preset.lamps);
@@ -20,6 +20,8 @@ export default function PresetCardComponent(props) {
 
     function handleClickDelete() {
         DeletePreset(preset); // TODO force a render somehow to delete
+        console.log("noja print");
+        parentForceUpdate();
     }
 
     return (
--- frontend/src/PresetsScreen.js
+++ frontend/src/PresetsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Grid from "@material-ui/core/Grid";
 import PresetCardComponent from "./PresetCardComponent";
 import { LoadPresets } from "./Util";
@@ -6,13 +6,20 @@ import { LoadPresets } from "./Util";
 export default function PresetsScreen(props) {
     const { ...other } = props;
 
+    const [, updateState] = React.useState();
+    const forceUpdate = useCallback(() => updateState({}), []);
+
     let presets = DefaultPresets().concat(LoadPresets());
 
     return (
         <Grid container spacing={3}>
             {presets.map((p, index) => (
                 <Grid item xs={3} key={index}>
-                    <PresetCardComponent {...other} preset={p} />
+                    <PresetCardComponent
+                        {...other}
+                        parentForceUpdate={forceUpdate}
+                        preset={p}
+                    />
                 </Grid>
             ))}
         </Grid>
